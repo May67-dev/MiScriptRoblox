c77f69ce746dc8cf2f31d8d8ca13c68c250846bb
May67-dev <maycolernesto0@gmail.com>
2026-08-03 12:43:12 +0200

    update main.lua

---
+++ main.lua
@@ -0,0 +1,38 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "Nc Hub | Nc Script",
+   LoadingTitle = "Cargando Script...",
+   LoadingSubtitle = "Nc Script",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = "MisScripts",
+      FileName = "ConfigPrincipal"
+   },
+   KeySystem = false -- Ponlo en true si quieres ponerle contraseña
+})
+
+-- Pestaña de Información (Igual a la de tu foto)
+local InfoTab = Window:CreateTab("Info", 4483362458) -- Icono de Info
+
+local Section = InfoTab:CreateSection("Discord")
+
+InfoTab:CreateButton({
+   Name = "Próximamente 👻",
+   Callback = function()
+      -- Aquí pondrías el link
+      print("Link copiado")
+   end,
+})
+
+-- Pestaña de Funciones
+local MainTab = Window:CreateTab("Main", 4483362458) -- Icono de Gamepad
+
+MainTab:CreateButton({
+   Name = "Velocidad (Speed) 100",
+   Callback = function()
+      game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 100
+   end,
+})
+
+-- Barra de búsqueda (Rayfield la trae por defecto en las pestañas)
